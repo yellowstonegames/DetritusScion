db6d5430091deec620e36391dd22ab0d1488eff7
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-03-25 20:24:13 -0700

    Bring over a skeleton project.

---
+++ core/src/main/java/com/github/yellowstonegames/Main.java
@@ -0,0 +1,7 @@
+package com.github.yellowstonegames;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+}
