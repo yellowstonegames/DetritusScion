00b47f5501f48d54c3efd3521b6b6596c472ff5c
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-03-26 20:12:11 -0700

    Bring over a functioning DungeonDemo.

--- lwjgl3/src/main/java/com/github/yellowstonegames/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/github/yellowstonegames/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,9 @@ package com.github.yellowstonegames.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.github.yellowstonegames.Main;
+import com.github.yellowstonegames.DungeonDemo;
+
+import static com.github.yellowstonegames.DungeonDemo.*;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -11,7 +13,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+        return new Lwjgl3Application(new DungeonDemo(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
@@ -23,7 +25,7 @@ public class Lwjgl3Launcher {
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(640, 480);
+        configuration.setWindowedMode(CELL_WIDTH * GRID_WIDTH, CELL_HEIGHT * GRID_HEIGHT);
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
