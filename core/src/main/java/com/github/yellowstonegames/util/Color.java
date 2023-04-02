e5ed4c6cd99925cb1f0268a559ce5c33e62650a2
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-04-01 21:38:40 -0700

    Change colors to int.

--- core/src/main/java/com/github/yellowstonegames/util/Color.java
+++ core/src/main/java/com/github/yellowstonegames/util/Color.java
@@ -1,18 +1,29 @@
 package com.github.yellowstonegames.util;
 
+import com.github.yellowstonegames.core.DescriptiveColor;
+import com.github.yellowstonegames.core.FullPalette;
+
+/**
+ * Try to use int colors, either Oklab (using the methods in DescriptiveColor to edit them) or RGBA.
+ */
 public class Color {
 
-    public static com.badlogic.gdx.graphics.Color[][] randomColors(int innerSize, RNG rng) {
-    com.badlogic.gdx.graphics.Color[][] cs = new com.badlogic.gdx.graphics.Color[8][innerSize];
+    private static final int START_CW = FullPalette.LIST.indexOf(FullPalette.CW_RED),
+        END_CW = FullPalette.LIST.indexOf(FullPalette.CW_WHITE)+1;
+
+    // Tommy: This probably isn't equivalent to what was here before; I'm working on it.
+    // Before, this used randomColorWheel(IRNG), and that samples a limited palette, oddly enough .
+    public static int[][] randomColors(int innerSize, RNG rng) {
+    int[][] cs = new int[8][innerSize];
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < innerSize; j++) {
-            cs[i][j] = SColor.randomColorWheel(rng);
+            cs[i][j] = FullPalette.LIST.get(rng.nextInt(START_CW, END_CW));
         }
     }
     return cs;
 }
 
-    public static float progressiveLighten(float color) {
-        return SColor.toEditedFloat(color, 0f, 0f, (1.0f - SColor.lumaOfFloat(color)) * 0.4f, 1f);
+    public static int progressiveLighten(int color) {
+        return DescriptiveColor.lighten(color, (1f - DescriptiveColor.lightness(color)) * 0.4f);
     }
 }
