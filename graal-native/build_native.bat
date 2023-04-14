d9267701afc934ecf6d708c1bc20556640d13842
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-04-14 00:12:24 -0700

    Get the .bat file in here!

---
+++ graal-native/build_native.bat
@@ -0,0 +1,8 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64 && ^
+C:\d\jvm\graal11\bin\native-image.cmd ^
+-H:+ReportExceptionStackTraces ^
+--report-unsupported-elements-at-runtime ^
+--no-fallback ^
+-H:IncludeResources=".*.dll" ^
+-Dorg.lwjgl.librarypath=. ^
+-jar DetritusScion.jar 
