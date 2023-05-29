03d6bd25605b02cf07a2fb4f04858db1714383b5
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-05-29 15:20:42 -0700

    Almost got GraalHelper avoiding AutomaticFeature.

--- lwjgl3/src/main/java/com/github/yellowstonegames/lwjgl3/ReflectionConfiguration.java
+++ lwjgl3/src/main/java/com/github/yellowstonegames/lwjgl3/ReflectionConfiguration.java
@@ -1,13 +1,11 @@
 package com.github.yellowstonegames.lwjgl3;
 
 import com.github.yellowstonegames.glyph.GridAction;
-import com.oracle.svm.core.annotate.AutomaticFeature;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
 import com.github.yellowstonegames.glyph.MoreActions;
 
-@AutomaticFeature
 public class ReflectionConfiguration implements Feature {
     @Override
     public void beforeAnalysis(BeforeAnalysisAccess access) {
