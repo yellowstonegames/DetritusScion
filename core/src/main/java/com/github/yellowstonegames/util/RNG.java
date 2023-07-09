b7aed79c49b6ccf97495e24f45213f019c131af9
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-07-08 22:33:19 -0700

    RNG cleanup in the wake of Manifold removal.

--- core/src/main/java/com/github/yellowstonegames/util/RNG.java
+++ core/src/main/java/com/github/yellowstonegames/util/RNG.java
@@ -1,11 +1,11 @@
 package com.github.yellowstonegames.util;
 
+import com.github.tommyettinger.random.AceRandom;
 import com.github.tommyettinger.random.Deserializer;
-import com.github.tommyettinger.random.WhiskerRandom;
 
 import java.util.List;
 
-public class RNG extends WhiskerRandom {
+public class RNG extends AceRandom {
 
     /**
      * A publicly available rng to allow for use without constructing.
@@ -14,14 +14,14 @@ public class RNG extends WhiskerRandom {
     public static final RNG rng = new RNG();
 
     /**
-     * Creates a new WhiskerRandom with a random state.
+     * Creates a new RNG with a random state.
      */
     public RNG() {
         super();
     }
 
     /**
-     * Creates a new WhiskerRandom with the given seed; all {@code long} values are permitted.
+     * Creates a new RNG with the given seed; all {@code long} values are permitted.
      * The seed will be passed to {@link #setSeed(long)} to attempt to adequately distribute the seed randomly.
      *
      * @param seed any {@code long} value
@@ -31,16 +31,65 @@ public class RNG extends WhiskerRandom {
     }
 
     /**
-     * Creates a new WhiskerRandom with the given four states; all {@code long} values are permitted.
+     * Creates a new RNG with the given five states; all {@code long} values are permitted.
      * These states will be used verbatim.
      *
      * @param stateA any {@code long} value
      * @param stateB any {@code long} value
      * @param stateC any {@code long} value
      * @param stateD any {@code long} value
+     * @param stateE any {@code long} value
      */
-    public RNG(long stateA, long stateB, long stateC, long stateD) {
-        super(stateA, stateB, stateC, stateD);
+    public RNG(long stateA, long stateB, long stateC, long stateD, long stateE) {
+        super(stateA, stateB, stateC, stateD, stateE);
+    }
+
+    /**
+     * This initializes all 5 states of the generator to random values based on the given seed.
+     * (2 to the 64) possible initial generator states can be produced here.
+     *
+     * @param seed the initial seed; may be any long
+     */
+    @Override
+    public void setSeed (long seed) {
+        // Based on (not identical to) the Speck block cipher's key expansion.
+        // Only uses add, bitwise rotation, and XOR operations.
+        long s0 = seed, s1 = seed ^ 0xC6BC279692B5C323L, ctr = seed ^ 0x1C69B3F74AC4AE35L;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr += 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateA = s0;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr += 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateB = s0;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr += 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateC = s0;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr += 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateD = s0;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr += 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateE = s0;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr += 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateA += s0;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr += 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateB += s0;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr += 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateC += s0;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr += 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateD += s0;
+        s1 = (s1 << 56 | s1 >>>  8) + s0 ^ (ctr + 0xBEA225F9EB34556DL);
+        s0 = (s0 <<  3 | s0 >>> 61) ^ s1;
+        stateE += s0;
+    }
+
+    @Override
+    public RNG copy() {
+        return new RNG(stateA, stateB, stateC, stateD, stateE);
     }
 
     /**
@@ -123,6 +172,51 @@ public class RNG extends WhiskerRandom {
             return null;
         }
         return randomElement(list);
+    }
+
+    public boolean randomElement(boolean[] arr){
+        if (arr == null || arr.length == 0) return false;
+        return arr[nextInt(arr.length)];
+    }
+
+    public byte randomElement(byte[] arr){
+        if (arr == null || arr.length == 0) return 0;
+        return arr[nextInt(arr.length)];
+    }
+
+    public short randomElement(short[] arr){
+        if (arr == null || arr.length == 0) return 0;
+        return arr[nextInt(arr.length)];
+    }
+
+    public int randomElement(int[] arr){
+        if (arr == null || arr.length == 0) return 0;
+        return arr[nextInt(arr.length)];
+    }
+
+    public long randomElement(long[] arr){
+        if (arr == null || arr.length == 0) return 0;
+        return arr[nextInt(arr.length)];
+    }
+
+    public float randomElement(float[] arr){
+        if (arr == null || arr.length == 0) return 0;
+        return arr[nextInt(arr.length)];
+    }
+
+    public double randomElement(double[] arr){
+        if (arr == null || arr.length == 0) return 0;
+        return arr[nextInt(arr.length)];
+    }
+
+    public char randomElement(char[] arr){
+        if (arr == null || arr.length == 0) return '\u0000';
+        return arr[nextInt(arr.length)];
+    }
+
+    public char randomElement(CharSequence str){
+        if (str == null || str.isEmpty()) return '\u0000';
+        return str.charAt(nextInt(str.length()));
     }
 
     // Serialization code
