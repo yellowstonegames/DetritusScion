82854f5a0dca4787c05452160bc7f4688ac62e4f
Berstanio <berstaniodev@gmail.com>
2023-10-12 15:03:38 +0200

    fix: Switch to using the native-image gradle plugin

--- graal-native/build_native.bat
+++
@@ -1,5 +0,0 @@
-C:\d\jvm\graal17\bin\native-image.cmd ^
--Dfile.encoding=UTF8 ^
--H:IncludeResources="((fonts|images|music|sfx|shaders)/.*)|(.*\.dll)|(com/badlogic/gdx/utils/.*\.(png|fnt))" ^
--march=compatibility ^
--jar ../lwjgl3/build/lib/DetritusScion.jar
